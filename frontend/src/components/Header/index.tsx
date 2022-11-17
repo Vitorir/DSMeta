78b831cd569bd8ec50f82339c6921453cde431d7
Vitorir <vitorireboucas@edu.unifor.br>
2022-11-16 23:31:11 -0300

    componentizacao

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+
+function header() {
+    return(
+      <>
+<header>
+<div className="dsmeta-logo-container">
+    <img src="Logo.svg" alt="DSMeta"/>
+    <h1>DSMeta</h1>
+    <p>Desenvolvido por 
+        <a href="https://www.instagram.com/devsuperior.ig">@devsuperior</a>
+    </p>
+</div>
+</header>
+      </>
+    )  
+  }
+  
+  export default header;
+  
