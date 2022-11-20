4128b92a7244dc14f8b2d8c7e23ba912dc9c446b
Vitorir <vitorireboucas@edu.unifor.br>
2022-11-20 06:11:30 -0300

    Other Components

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import logo from '../../assets/img/Logo.svg'
+import './styles.css'
 
 function header() {
     return(
       <>
 <header>
 <div className="dsmeta-logo-container">
-    <img src="Logo.svg" alt="DSMeta"/>
+    <img src={logo} alt="DSMeta"/>
     <h1>DSMeta</h1>
     <p>Desenvolvido por 
         <a href="https://www.instagram.com/devsuperior.ig">@devsuperior</a>
